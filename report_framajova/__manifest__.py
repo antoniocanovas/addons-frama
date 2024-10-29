a0496abef8bd47fd520ccea3a138f3fe842f67e4
arquero20022 <amm2002ct@gmail.com>
2024-10-29 12:08:53 +0000

    reports

---
+++ report_framajova/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Punt Sistemes SL
+#    Copyright (C) 2024 - Punt Sistemes (http://www.puntsistemes.es). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "report framajova",
+    "version": "1.0",
+    "depends": [],
+    "author": "Punt Sistemes",
+    "category": "Project",
+    "website": "https://www.puntsistemes.es",
+    "description": """
+        This module links TASKS and SIGN REQUEST. 
+    """,
+    "data": [
+        "data/page_format.xml",
+        "reports/frama_stock_report_delivery_has_serial_move_line.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
